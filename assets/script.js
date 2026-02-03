2c1a726596c8fc8623203bd137f9822057da384a
yun-tg <guren940629@gmail.com>
2026-02-03 23:18:19 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,3 @@ document.addEventListener("DOMContentLoaded", function() {
   submenu.forEach(item => {
     item.addEventListener('click', function(event) {
       const submenuItems = this.nextElementSibling;
-      const parentMenu = this.parentElement;
-
-      // 如果当前菜单项有子菜单，切换其显示状态
-      if (submenuItems.style.display === 'block') {
-        submenuItems.style.display = 'none';
-        parentMenu.classList.remove('active');
-      } else {
-        submenuItems.style.display = 'block';
-        parentMenu.classList.add('active');
-      }
-
-      // 阻止事件冒泡，避免点击子菜单链接时触发父菜单
-      event.stopPropagation();
-    });
-  });
-
-  // 点击文档其他部分时收起所有子菜单
-  document.addEventListener('click', function(event)
