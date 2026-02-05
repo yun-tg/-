86cacb3db5bd57e0b1387893acf1c439e13f321f
yun-tg <guren940629@gmail.com>
2026-02-05 19:25:10 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,28 +4,22 @@ async function loadMarkdown(file) {
   article.innerHTML = "";
 
   try {
-    // 先加载 top.md
-    const topRes = await fetch("tutorials/top.md");
-    const topText = await topRes.text();
-    article.innerHTML += marked.parse(topText);
+    const top = await fetch("tutorials/top.md");
+    article.innerHTML += marked.parse(await top.text());
 
-    // 再加载点击的教程
     if (file) {
       const res = await fetch("tutorials/" + file);
-      const text = await res.text();
-      article.innerHTML += marked.parse(text);
+      article.innerHTML += marked.parse(await res.text());
     }
   } catch {
     article.innerHTML = "教程加载失败";
   }
 }
 
-// 绑定子菜单点击
 document.querySelectorAll(".submenu li").forEach(li => {
   li.addEventListener("click", () => {
     loadMarkdown(li.dataset.md);
   });
 });
 
-// 默认加载
 loadMarkdown();
